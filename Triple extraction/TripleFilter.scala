1c4802119090bf64125a0eae74118a0cddf09293
AyoubRAHOUI <32583919+AyoubRAHOUI@users.noreply.github.com>
2019-10-10 15:05:58 +0200

    Adding last python scripts for scala classes

---
+++ Triple extraction/TripleFilter.scala
@@ -0,0 +1,42 @@
+
+
+
+object TripleFilter {
+  val commonVerbs = Set("is", "was")
+
+  def filterRelation(relation: String): Boolean = { 
+    // if (badVerbs.contains(relation) || relation.startsWith("'s")) 
+    if (relation.startsWith("'s")) 
+      false
+    else
+      true
+  }   
+  
+  def filterEntities(
+    namedPhrases: Set[String], 
+          sub: String, 
+          obj: String): Boolean = { 
+    namedPhrases.exists(n => sub.contains(n)) 
+  }   
+
+  def filterCommonVerbs(triple: Triple, namedPhrases: Set[String]): Boolean = {
+    if (commonVerbs.contains(triple.pred)) {
+      if (namedPhrases.exists(n => triple.obj.contains(n))) 
+        true
+      else 
+        false
+    }
+    else 
+      true
+  }
+
+  def filter(triple: Triple, namedPhrases: Set[String]): Boolean = {
+    if (filterRelation(triple.pred) == false)
+      return false
+    if (filterEntities(namedPhrases, triple.sub, triple.obj) == false) 
+      return false
+    if (filterCommonVerbs(triple, namedPhrases) == false) 
+      return false
+    true 
+  }
+}
