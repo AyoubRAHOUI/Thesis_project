7ff8623933ef05054b7f3eadd842c184a56aa6d2
AyoubRAHOUI <32583919+AyoubRAHOUI@users.noreply.github.com>
2019-10-10 15:06:40 +0200

    adding modules

---
+++ Entity disambiguation/GraphProp.scala
@@ -0,0 +1,10 @@
+
+object GraphProp {
+
+  val edgeLabelNeighbourEntity = "linksTo"
+  val edgeLabelNodeType = "rdf:type"
+  val edgeLabelNodeAlias : List[String]= List("rdfs:label", "skos:prefLabel", "isPreferredMeaningOf")
+  val aliasSep: String = " <Alias> "
+  val typeSep :String = " <Type> "
+
+}
